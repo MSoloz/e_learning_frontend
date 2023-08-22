c83469884ebb5c299aa62c75a9a4900ef93a258d
oumaima176 <87426271+oumaima176@users.noreply.github.com>
2023-08-22 13:59:56 +0100

    signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
 //
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  
+  { 
+    path:'',component: SigninComponent
+  },
 ];
 
 const routeOptions: ExtraOptions = {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 //
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //NO LONGER SUPPORTED: import { FlexLayoutModule } from '@angular/flex-layout';
@@ -10,19 +11,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { SigninComponent } from './components/signin/signin.component';
 // routed components
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
+    SigninComponent,
+    
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
-
+    ReactiveFormsModule,
   ],
   providers: [
     {
